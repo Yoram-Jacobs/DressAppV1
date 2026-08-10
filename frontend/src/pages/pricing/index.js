4e5b011c5f51ed13e60cad69fdc5ef880b2de1b2
Yoram-Jacobs <lokoprod@gmail.com>
2026-08-10 21:46:13 +0300

    refactor: prune 42 dead backup/temporary/obsolete credit files and unreachable code

--- frontend/src/pages/pricing/index.js
+++
@@ -1,4 +0,0 @@
-export { QuotaMonitor } from './QuotaMonitor.jsx';
-export { PricingDisplay } from './PricingDisplay.jsx';
-export { CreditPackPurchase } from './CreditPackPurchase.jsx';
-export { useSubscriptionManager } from './useSubscriptionManager.js';
